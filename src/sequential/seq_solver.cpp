0c786c3cb6406bdde6121908c792eaae03f9d6ac
pwnsauce8 <32200668+pwnsauce8@users.noreply.github.com>
2022-11-08 21:10:07 +0100

    Refactor

--- src/sequential/seq_solver.cpp
+++ src/sequential/seq_solver.cpp
@@ -1,7 +1,6 @@
 #include "seq_solver.h"
 #include "../rss/statistics.cpp"
 #include "../file_mapping.h"
-#include "../file_mapping.cpp"
 #include "../histogram/histogram.cpp"
 
 namespace ppr::seq
@@ -11,7 +10,7 @@ namespace ppr::seq
 		SResult result;
 		FileMapping mapping(configuration.input_fn);
 
-		const double* data = mapping.get_data();
+		const double* data = mapping.GetData();
 
 		if (!data)
 		{
@@ -21,27 +20,26 @@ namespace ppr::seq
 		RunningStat stat(data[0]);
 
 		// Get statistics
-		for (unsigned int i = 1; i < mapping.get_count(); i++)
+		for (unsigned int i = 1; i < mapping.GetCount(); i++)
 		{
 			double d = (double)data[i];
 			stat.Push(d);
 		}
 
 		// Create histogram
-		double bin_count = log2(mapping.get_count()) + 1;
+		double bin_count = log2(mapping.GetCount()) + 1;
 		double bin_size = (stat.Get_Max() - stat.Get_Min()) / bin_count;
 
 		ppr::hist::Histogram hist(static_cast<int>(bin_count) + 1, bin_size, stat.Get_Min());
-		for (unsigned int i = 0; i < mapping.get_count(); i++)
+		for (unsigned int i = 0; i < mapping.GetCount(); i++)
 		{
 			double d = (double)data[i];
 			hist.Push(d);
 		}
-		mapping.unmap_file();
+		mapping.UnmapFile();
 
 		// Get propability density of histogram
-		hist.FindBucketEdges();
-		hist.ComputePropabilityDensityOfHistogram(mapping.get_count());
+		hist.ComputePropabilityDensityOfHistogram(mapping.GetCount());
 
 		// Fit params
 		// ================ [Gauss maximum likelihood estimators]
@@ -61,11 +59,30 @@ namespace ppr::seq
 
 
 		// Calculate PDF for params
-
 		double g_rss = hist.ComputeRssOfHistogram('n', gauss_mean, gauss_sd);
 		double e_rss = hist.ComputeRssOfHistogram('e', 0.0, 0.0, exp_lambda);
 		double p_rss = hist.ComputeRssOfHistogram('p', 0.0, 0.0, 0.0, poisson_lambda);
 		double u_rss = hist.ComputeRssOfHistogram('u', 0.0, 0.0, 0.0, 0.0, a, b);
+
+		/*double min = std::min({ g_rss, e_rss, p_rss, u_rss });
+
+		if (gauss_rss == max)
+		{
+			std::cout << "This is Gauss" << std::endl;
+		}
+		else if (exp_rss == max)
+		{
+			std::cout << "This is Exponential" << std::endl;
+		}
+		else if (poisson_rss == max)
+		{
+			std::cout << "This is Poisson" << std::endl;
+		}
+		else if (uniform_rss == max)
+		{
+			std::cout << "This is Uniform" << std::endl;
+		}
+*/
 
 
 		return SResult::error_res(EExitStatus::STAT);
