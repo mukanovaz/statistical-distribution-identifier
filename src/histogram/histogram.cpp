736d5ed83faa30bb6ed3663d8de8be16741ed40f
pwnsauce8 <32200668+pwnsauce8@users.noreply.github.com>
2022-11-11 15:55:22 +0100

    Refactor histogram

--- src/histogram/histogram.cpp
+++ src/histogram/histogram.cpp
@@ -9,57 +9,50 @@ namespace ppr::hist
 	{
 		private:
 			double BinSize;
-			double Min, Size;
-			std::unique_ptr<double[]> BucketsDensity;
-			std::unique_ptr<double[]> BucketFrequency;
-			std::unique_ptr<double[]> BucketEdges;
+			double Max, Min, Size, ScaleFactor;
 
 		public:
-			Histogram(int size, double bin_size, double min)
-				: Size(size), BinSize(bin_size), Min(min)
+			Histogram(int size, double bin_size, double min, double max)
+				: Size(size), BinSize(bin_size), Min(min), Max(max), ScaleFactor(0.0)
 			{
-				BucketFrequency = std::make_unique<double[]>(size);
-				BucketsDensity = std::make_unique<double[]>(size);
-				BucketEdges = std::make_unique<double[]>(size);
-				//Buckets.reserve()
-				/*double bin_count = log2(mapping.get_count()) + 1;
-				double bin_size = (stat.Get_Max() - stat.Get_Min()) / bin_count;
-				std::vector<uint64_t> buckets(static_cast<int>(bin_count) + 1);*/
+				ScaleFactor = (Size) / (Max - Min);
 			}
 
-            void Push(double x)
-            {
-				double position = ((x - Min) / BinSize);
-				BucketFrequency[static_cast<int>(position)]++;
-            }
+			void Push(std::vector<int>& arr, double x)
+			{
+				double position = (x - Min) * ScaleFactor;
+				if (position == arr.size())
+				{
+					position--;
+				}
+				arr[static_cast<int>(position)]++;
+			}
 
-			void FindBucketEdges()
+			void ComputePropabilityDensityOfHistogram(std::vector<double>& bucket_density, std::vector<int>& bucket_frequency, double count)
 			{
 				for (unsigned int i = 0; i < Size; i++)
 				{
-					BucketEdges[i] = BinSize * i;
-					//std::cout << BucketEdges[i] << std::endl;;
+					std::cout << bucket_frequency[i] << std::endl;
 				}
-			}
 
-			void ComputePropabilityDensityOfHistogram(double count)
-			{
-				for (unsigned int i = 0; i < Size - 1; i++)
+				for (unsigned int i = 0; i < Size; i++)
 				{
-					double diff = BucketEdges[i + 1] - BucketEdges[i];
-					BucketsDensity[i] = BucketFrequency[i] / diff/ count;
-					std::cout << BucketsDensity[i] << std::endl;;
+					double next_edge = Min + (BinSize * (static_cast<double>(i) + 1.0));
+					double curr_edge = Min + (BinSize * static_cast<double>(i));
+					double diff = next_edge - curr_edge;
+					bucket_density[i] = bucket_frequency[i] / diff / count;
+					std::cout << bucket_density[i] << std::endl;
 				}
 			}
 
-			double ComputeRssOfHistogram(char dist_val, double mean = 0.0, double stddev = 0.0, double lambda = 0.0, double mu = 0.0, double a = 0.0, double b = 0.0)
+			double ComputeRssOfHistogram(std::vector<double>& bucket_density, char dist_val, double variance = 0.0, double mean = 0.0, double stddev = 0.0, double lambda = 0.0, double mu = 0.0, double a = 0.0, double b = 0.0)
 			{
 				ppr::rss::Distribution* dist;
 
 				switch (dist_val)
 				{
 					case 'n':
-						dist = new ppr::rss::NormalDistribution(mean, stddev);
+						dist = new ppr::rss::NormalDistribution(mean, stddev, variance);
 						break;
 					case 'e':
 						dist = new ppr::rss::ExponentialDistribution(lambda);
@@ -73,9 +66,10 @@ namespace ppr::hist
 					default:
 						return 0.0;
 				}
+
 				for (unsigned int i = 0; i < Size; i++)
 				{
-					double d = (double)BucketsDensity[i];
+					double d = (double)bucket_density[i];
 					dist->Push(d, (i * BinSize));
 				}
 				
