bae19262fa208a3b139e5becd41734c895f5b720
pwnsauce8 <32200668+pwnsauce8@users.noreply.github.com>
2022-11-30 14:09:10 +0100

    added vectorization

--- src/smp/smp_solver.cpp
+++ src/smp/smp_solver.cpp
@@ -82,11 +82,113 @@ namespace ppr::parallel
 		return res;
 	}
 
+	void avx2_sum_64_block(SDataStat& stat, double* vec_first, double* vec_second, __m256d& max, __m256d& min) noexcept
+	{
+		__m256d accumulator1 = _mm256_setzero_pd();
+		__m256d accumulator2 = _mm256_setzero_pd();
+
+		for (std::size_t block = 0; block < 64; block += 4 * 2)
+		{
+			__m256d vec_f1 = _mm256_loadu_pd(vec_first + block);
+			__m256d vec_f2 = _mm256_loadu_pd(vec_first + block + 4);
+			__m256d vec_s1 = _mm256_loadu_pd(vec_second + block);
+			__m256d vec_s2 = _mm256_loadu_pd(vec_second + block + 4);
+
+			__m256d tmp1 = _mm256_add_pd(vec_f1, vec_s1);
+			__m256d tmp2 = _mm256_add_pd(vec_f2, vec_s2);
+			accumulator1 = _mm256_add_pd(accumulator1, tmp1);
+			accumulator2 = _mm256_add_pd(accumulator2, tmp2);
+
+			max = _mm256_max_pd(max, vec_f1);
+			max = _mm256_max_pd(max, vec_f2);
+			max = _mm256_max_pd(max, vec_s1);
+			max = _mm256_max_pd(max, vec_s2);
+
+			min = _mm256_min_pd(min, vec_f1);
+			min = _mm256_min_pd(min, vec_f2);
+			min = _mm256_min_pd(min, vec_s1);
+			min = _mm256_min_pd(min, vec_s2);
+		}
+
+		accumulator1 = _mm256_add_pd(accumulator1, accumulator2);
+
+		__m128d accumulator = _mm_add_pd(_mm256_castpd256_pd128(accumulator1),
+			_mm256_extractf128_pd(accumulator1, 1));
+		accumulator = _mm_add_pd(accumulator,
+			_mm_unpackhi_pd(accumulator, accumulator));
+
+		stat.sum += _mm_cvtsd_f64(accumulator);
+	}
+
+
+	// https://stackoverflow.com/questions/49941645/get-sum-of-values-stored-in-m256d-with-sse-avx
+	inline
+		double hsum_double_avx(__m256d v) {
+		__m128d vlow = _mm256_castpd256_pd128(v);
+		__m128d vhigh = _mm256_extractf128_pd(v, 1); // high 128
+		vlow = _mm_add_pd(vlow, vhigh);     // reduce down to 128
+
+		__m128d high64 = _mm_unpackhi_pd(vlow, vlow);
+		return  _mm_cvtsd_f64(_mm_add_sd(vlow, high64));  // reduce to scalar
+	}
+
+
+	// https://stackoverflow.com/questions/13219146/how-to-sum-m256-horizontally
+	void avx2_sum_64_block2(double& sum, double* vec_first, double* vec_second) noexcept
+	{
+		__m256d accumulator1 = _mm256_setzero_pd();
+		__m256d accumulator2 = _mm256_setzero_pd();
+
+		for (std::size_t block = 0; block < 64; block += 4)
+		{
+			__m256d vec_f1 = _mm256_loadu_pd(vec_first + block);
+			__m256d vec_s1 = _mm256_loadu_pd(vec_second + block);
+
+			double s1 = hsum_double_avx(vec_f1);
+			double s2 = hsum_double_avx(vec_s1);
+
+			sum += s1 + s2;
+		}
+	}
+
+	void GetStatisticsVectorized(SHistogram& hist, SConfig& configuration, SDataStat& stat, unsigned int data_count, double* data, std::vector<int>& histogram)
+	{
+		int half_count = data_count / 2;
+		__m256d min = _mm256_set_pd(
+			std::numeric_limits<double>::max(),
+			std::numeric_limits<double>::max(),
+			std::numeric_limits<double>::max(),
+			std::numeric_limits<double>::max()
+		);
+
+		__m256d max = _mm256_set_pd(
+			std::numeric_limits<double>::lowest(),
+			std::numeric_limits<double>::lowest(),
+			std::numeric_limits<double>::lowest(),
+			std::numeric_limits<double>::lowest()
+		);
+
+ 		for (int block = 0; block < half_count; block += 64)
+		{
+			avx2_sum_64_block(stat, data + block, data + block + half_count, max, min);
+		}
+		double test = 0.0;
+	}
+
 	void GetStatisticsCPU(SHistogram& hist, SConfig& configuration, SOpenCLConfig& opencl, SDataStat& stat, tbb::task_arena& arena, unsigned int data_count, double* data, std::vector<int>& histogram)
 	{
+		if (USE_VECTORIZATION)
+		{
+			tbb::tick_count t0 = tbb::tick_count::now();
+			GetStatisticsVectorized(hist, configuration, stat, data_count, data, histogram);
+			tbb::tick_count t1 = tbb::tick_count::now();
+			std::cout << (t1 - t0).seconds() << std::endl;
+		}
+		else
+		{
 			// Find rest of a statistics on CPU
 			RunningStatParallel stat_cpu(data, opencl.data_count_for_cpu);
-			ppr::executor::RunOnCPU<RunningStatParallel>(arena, stat_cpu, opencl.data_count_for_cpu, data_count);
+			ppr::executor::RunOnCPU<RunningStatParallel>(arena, stat_cpu, opencl.data_count_for_cpu + 1, data_count);
 
 			// Agregate results results
 			stat.n += stat_cpu.NumDataValues();
@@ -94,6 +196,7 @@ namespace ppr::parallel
 			stat.max = std::max({ stat.max, std::max({ stat.max, stat_cpu.Get_Max() }) });
 			stat.sum += stat_cpu.Sum();
 			stat.isNegative = stat.isNegative || stat_cpu.IsNegative();
+		}
 	}
 
 	void CreateFrequencyHistogramCPU(SHistogram& hist, SConfig& configuration, SOpenCLConfig& opencl, SDataStat& stat, tbb::task_arena& arena, unsigned int data_count, double* data, std::vector<int>& histogram)
--- src/smp/smp_solver.h
+++ src/smp/smp_solver.h
@@ -8,6 +8,6 @@ namespace ppr::parallel
 {
 	SResult run(SConfig& configuration);
 	SResult run(SConfig& configuration);
-	void GetStatisticsCPU(SHistogram& hist, SConfig& configuration, SOpenCLConfig& opencl, SDataStat& stat, tbb::task_arena& arena, unsigned int data_count, double* data, std::vector<int>& histogram);
+	SDataStat GetStatisticsCPU(SHistogram& hist, SConfig& configuration, SOpenCLConfig& opencl, tbb::task_arena& arena, unsigned int data_count, double* data);
 	void CreateFrequencyHistogramCPU(SHistogram& hist, SConfig& configuration, SOpenCLConfig& opencl, SDataStat& stat, tbb::task_arena& arena, unsigned int data_count, double* data, std::vector<int>& histogram);
 }
