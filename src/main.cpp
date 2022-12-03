7b10a241ef3ab2c1f72997e0d956a0bf20a5de08
pwnsauce8 <32200668+pwnsauce8@users.noreply.github.com>
2022-12-03 11:29:44 +0100

    Changed outputs

--- src/main.cpp
+++ src/main.cpp
@@ -27,16 +27,16 @@ void GetDistString(SResult result)
 {
 	switch (result.dist) {
 		case EDistribution::GAUSS: 
-			std::cout << "Input data have 'Gauss/Normal distribution' with mean=" << result.gauss_mean << " and variance=" << result.gauss_variance << std::endl;
+			std::cout << "> Input data have 'Gauss/Normal distribution' with mean=" << result.gauss_mean << " and variance=" << result.gauss_variance << std::endl;
 			break;
 		case EDistribution::POISSON: 
-			std::cout << "Input data have 'Poisson distribution' with lambda=" << result.poisson_lambda << result.gauss_variance << std::endl;
+			std::cout << "> Input data have 'Poisson distribution' with lambda=" << result.poisson_lambda << result.gauss_variance << std::endl;
 			break;
 		case EDistribution::EXP: 
-			std::cout << "Input data have 'Exponential distribution' with lambda=" << result.exp_lambda << result.gauss_variance << std::endl;
+			std::cout << "> Input data have 'Exponential distribution' with lambda=" << result.exp_lambda << result.gauss_variance << std::endl;
 			break;
 		case EDistribution::UNIFORM: 
-			std::cout << "Input data have 'Uniform distribution' with a=" << result.uniform_a << " and b=" << result.uniform_b << std::endl;
+			std::cout << "> Input data have 'Uniform distribution' with a=" << result.uniform_a << " and b=" << result.uniform_b << std::endl;
 			break;
 		default: "Unknown";
 	}
@@ -49,11 +49,50 @@ int main(int argc, char** argv) {
 	{
 		return ppr::EExitStatus::ARGS;
 	}
+	SYSTEM_INFO sysInfo;
 
+	GetSystemInfo(&sysInfo);
+
+	printf("%s %d\n\n", "PageSize[Bytes] :", sysInfo.dwPageSize);
+	std::string mode = conf.mode == ppr::ERun_mode::SMP ? "smp" : "all";
+	std::string opt = ppr::USE_OPTIMIZATION ? "TRUE" : "FALSEs";
+
+	std::cout << "\t\t\t[Initial parameters]" << std::endl;
+	std::cout << "---------------------------------------------------------------------" << std::endl;
+	std::cout << "> Min chunk size:\t\t" << ppr::MAX_FILE_SIZE_MEM << " bytes" << std::endl;
+	std::cout << "> Optimalization:\t\t" << opt << std::endl;
+	std::cout << "> Number of threads:\t\t" << conf.thread_count << std::endl;
+	std::cout << "> Mode:\t\t\t\t" << mode << std::endl;
+	std::cout << "> Watchdog timer:\t\t" << ppr::WATCHDOG_INTERVAL_SEC << " sec" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "> Starting .." << std::endl;
 	SResult result = run(conf);
+	std::cout << "> Finish" << std::endl;
+	std::cout << std::endl;
 
+	std::cout << "\t\t\t[Time]" << std::endl;
+	std::cout << "---------------------------------------------------------------------" << std::endl;
+	std::cout << "> Statistics computing time:\t" << result.total_stat_time << " sec." << std::endl;
+	std::cout << "> Histogram computing time:\t" << result.total_hist_time << " sec." << std::endl;
+	std::cout << "> RSS computing time:\t\t" << result.total_rss_time << " sec." << std::endl;
+	std::cout << "> TOTAL TIME:\t\t\t" << result.total_time << " sec." << std::endl;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "\t\t\t[Results]" << std::endl;
+	std::cout << "---------------------------------------------------------------------" << std::endl;
+	std::cout << "> Gauss RSS:\t\t\t" << result.gauss_rss << std::endl;
+	std::cout << "> Poisson RSS:\t\t\t" << result.poisson_rss << std::endl;
+	std::cout << "> Exponential RSS:\t\t" << result.exp_rss << std::endl;
+	std::cout << "> Uniform RSS:\t\t\t" << result.uniform_rss << std::endl;
+	std::cout << "> Contains negative:\t\t" << result.isNegative << std::endl;
+	std::cout << "> All integers:\t\t\t" << result.isInteger << std::endl;
+	
+	std::cout << std::endl;
 	GetDistString(result);
-	std::cout << "RSS [g/n:" << result.gauss_rss << " p:" << result.poisson_rss << " e:" << result.exp_rss << " u:" << result.uniform_rss << "]" << std::endl;
 
 	delete conf.input_fn;
 
